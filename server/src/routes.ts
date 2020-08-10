b0bbab9db200b6e071eb4a8f052e76ad4349d93a
Marcelo Diament <marcelodiament@gmail.com>
2020-08-10 00:08:53 -0300

    feature: create class route - partial

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,35 @@
 import express from 'express';
+import db from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: "Hello World!" });
+routes.post('/classes', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = request.body;
+
+  const insertedUsersIds = await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio
+  });
+
+  const user_id = insertedUsersIds[0];
+
+  await db('classes').insert({
+    subject,
+    cost,
+    user_id
+  });
+
+  return response.send();
 });
 
 export default routes;
