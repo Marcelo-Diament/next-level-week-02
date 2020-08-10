15ec264b47251b71bf77c5fb41841ad95a3dce89
Marcelo Diament <marcelodiament@gmail.com>
2020-08-09 22:47:44 -0300

    documentation: rotas, recursos, métodos e params

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,15 @@ import express from 'express';
 
 const app = express();
 
-app.listen();
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+  console.log(request.body);
+  const users = [
+    { name: 'Fulano', age: 25 },
+    { name: 'Ciclano', age: 52 },
+  ];
+  return response.json(users);
+});
+
+app.listen(3333);
