c82aac27093e085144d6d8c3a991a8781476dd4c
Marcelo Diament <marcelodiament@gmail.com>
2020-08-12 20:01:22 -0300

    feature: first routes for classes and users

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,7 +3,54 @@ import db from './database/connection';
 
 const routes = express.Router();
 
+routes.get('/classes',async (request, response) => {
+  const allClasses = await db('classes');
+  return response.json(allClasses);
+});
+routes.get('/classes/:id',async (request, response) => {
+  const classesId = request.params.id;
+  const classesItem = await (await db('classes').where('id', `${classesId}`));
+  return response.json(classesItem);
+});
 routes.post('/classes', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = request.body;
+
+  const insertedUsersIds = await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio
+  });
+
+  const user_id = insertedUsersIds[0];
+
+  await db('classes').insert({
+    subject,
+    cost,
+    user_id
+  });
+
+  return response.send();
+});
+
+routes.get('/users',async (request, response) => {
+  const allUsers = await db('users');
+  return response.json(allUsers);
+});
+routes.get('/users/:id',async (request, response) => {
+  const userId = request.params.id;
+  const user = await (await db('users').where('id', `${userId}`));
+  return response.json(user);
+});
+routes.post('/users', async (request, response) => {
   const {
     name,
     avatar,
