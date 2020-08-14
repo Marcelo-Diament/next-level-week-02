f9c75449c686721a9880607f70186050c602da04
Marcelo Diament <marcelodiament@gmail.com>
2020-08-14 17:48:18 -0300

    feature: list and filter classes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,10 @@ import ClassesController from './controllers/ClassesController';
 const routes = express.Router();
 const classesControllers = new ClassesController();
 
+routes.get('/classes-all', classesControllers.getAllClasses);
+routes.get('/class/:id', classesControllers.getClassById);
+
+routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
 
 export default routes;
