05475bfc83602688689b88519a9421d1fc0a6eb3
Marcelo Diament <marcelodiament@gmail.com>
2020-08-14 18:25:27 -0300

    improvement: returning total connections instead of its details

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -4,8 +4,9 @@ import db from '../database/connection';
 export default class ConnectionsController {
 
   async index(request: Request, response: Response) {
-    const connections = await db('connections').select();
-    return response.json(connections);
+    const connections = await db('connections').count('* as total');
+    const { total } = connections[0]
+    return response.json(total);
   }
 
   async create(request: Request, response: Response) {
