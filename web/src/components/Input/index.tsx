7f2dd5d127bd143a56c18a88042a4b47861e5961
Marcelo Diament <marcelodiament@gmail.com>
2020-08-14 20:07:58 -0300

    feature: class form partial

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,7 +1,4 @@
 import React, { InputHTMLAttributes } from 'react';
-import { Link } from 'react-router-dom';
-import logoImg from '../../assets/images/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
 import './styles.css';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 const TeacherForm = () => {
@@ -29,6 +30,44 @@ const TeacherForm = () => {
             required
           />
         </fieldset>
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input
+            name="subject"
+            label="Matéria"
+            required
+          />
+          <Input
+            name="cost"
+            label="Custo hora/aula"
+            required
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Horários Disponíveis</legend>
+          <Input
+            name="name"
+            label="Nome"
+            required
+          />
+          <Input
+            name="avatar"
+            label="Avatar"
+            required
+          />
+          <Input
+            name="whatsapp"
+            label="Whatsapp"
+            required
+          />
+        </fieldset>
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante" />
+            Importante!<br />Preencha todos os dados
+          </p>
+          <button type="button">Salvar Cadastro</button>
+        </footer>
       </main>
     </div>
   )
