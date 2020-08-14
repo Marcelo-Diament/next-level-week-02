4b5215b0cec319a4e44c6594ff65069383a6c279
Marcelo Diament <marcelodiament@gmail.com>
2020-08-14 19:26:07 -0300

    feature: teacher form initial structure

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -6,6 +6,7 @@ import './style.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = props => {
@@ -19,6 +20,7 @@ const PageHeader: React.FC<PageHeaderProps> = props => {
       </div>
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description && <p className="header-paragraph">{props.description}</p>}
         {props.children}
       </div>
     </header>
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,9 +5,27 @@ import './styles.css';
 const TeacherForm = () => {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas!">
-        Teste
-      </PageHeader>
+      <PageHeader
+        title="Que incrível que você quer dar aulas!"
+        description="O primeiro passo é preencher este formulário de inscrição"
+      />
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <div className="input-block">
+            <label htmlFor="name">Nome</label>
+            <input type="text" id="name" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
     </div>
   )
 }
